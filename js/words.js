e39b948c877af4ac67d7d89b22bc73149a868980
Hasanul Mukit <132732768+hasanulmukit@users.noreply.github.com>
2025-01-08 16:10:52 +0600

    Added Guess the word game files

---
+++ js/words.js
@@ -0,0 +1,118 @@
+let wordList = [
+    {
+        word: "avatar",
+        hint: "Epic Science Fiction Film"
+    },
+    {
+        word: "bridge",
+        hint: "Structure To Cross Over"
+    },
+    {
+        word: "chess",
+        hint: "Related To An Indoor Game"
+    },
+    {
+        word: "cloud",
+        hint: "Mass Of Condensed Water Vapor"
+    },
+    {
+        word: "coding",
+        hint: "Related To Programming"
+    },
+    {
+        word: "dubai",
+        hint: "Developed City Name"
+    },
+    {
+        word: "earth",
+        hint: "Planet We Live On"
+    },
+    {
+        word: "ebay",
+        hint: "Online Shopping Site"
+    },
+    {
+        word: "egypt",
+        hint: "A Country Name"
+    },
+    {
+        word: "email",
+        hint: "Related To Exchanging Messages"
+    },
+    {
+        word: "google",
+        hint: "Famous Search Engine"
+    },
+    {
+        word: "gold",
+        hint: "A Yellow Precious Metal"
+    },
+    {
+        word: "guitar",
+        hint: "A Musical Instrument"
+    },
+    {
+        word: "html",
+        hint: "Markup Language For The Web"
+    },
+    {
+        word: "idea",
+        hint: "A Thought Or Suggestion"
+    },
+    {
+        word: "island",
+        hint: "Land Surrounded By Water"
+    },
+    {
+        word: "java",
+        hint: "Programming Language"
+    },
+    {
+        word: "map",
+        hint: "Diagram Representation Of An Area"
+    },
+    {
+        word: "mars",
+        hint: "Planet Of Our Solar System"
+    },
+    {
+        word: "mobile",
+        hint: "An Electronic Device"
+    },
+    {
+        word: "ocean",
+        hint: "Largest Body Of Water"
+    },
+    {
+        word: "photo",
+        hint: "Representation Of A Person Or Scene"
+    },
+    {
+        word: "piano",
+        hint: "A Musical Instrument"
+    },
+    {
+        word: "python",
+        hint: "Programming Language"
+    },
+    {
+        word: "rain",
+        hint: "Related To Water"
+    },
+    {
+        word: "robot",
+        hint: "Machine Capable Of Carrying Out Tasks"
+    },
+    {
+        word: "server",
+        hint: "Related To Computer Or System"
+    },
+    {
+        word: "tesla",
+        hint: "Unit Of Magnetic Flux Density"
+    },
+    {
+        word: "venus",
+        hint: "Planet Of Our Solar System"
+    }
+];
